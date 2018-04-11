800158b924d5b59eab5309384dfa7df8ab6695e3
Ankit Gaurav <ankitgaurav@outlook.com>
2018-04-11 11:53:53 +0530

    Add db support and mongoose

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose.connect(
+  'mongodb://' 
+  + process.env.DB_USERNAME + ':' 
+  + process.env.DB_PASSWORD + '@' 
+  + process.env.DB_HOST + ':41489/' 
+  + process.env.DB_DATABASE
+);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("DB connection OK.");
+});
+const User = mongoose.model('User', { name: String });
+
+// const user1 = new User({ name: 'Zildjian' });
+// user1.save().then(() => console.log(`${user1.name} saved into db.`));
+User.find((err, user)=>{
+  if (err) console.log(err);
+  console.log(user + '\n')
+})
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/secure', (req, res, next)=>{
+  res.send("You are in a secure area now.");
+})
+
 module.exports = router;
