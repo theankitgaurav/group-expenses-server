bfaed248dde3b567757623b99430ac1da1e013bd
Ankit Gaurav <ankitgaurav@outlook.com>
2018-04-16 01:14:03 +0530

    Add registration and login validations

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
-const User = new mongoose.Schema({
-    username: String,
+const passwordHash = require('password-hash');
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true
+    },
     name: String,
-    password: String
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    }
 });
 
-module.exports = mongoose.model('UserModel', User);
+UserSchema.pre('save', function(next) {
+    const user = this;
+    user.password = passwordHash.generate(user.password);
+    next();
+})
+
+// checking if password is valid
+UserSchema.methods.isValidPassword = function(password) {
+    return (passwordHash.verify(password, this.password));
+};
+
+module.exports = mongoose.model('UserModel', UserSchema);
