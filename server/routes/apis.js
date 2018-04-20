eeb02ce4fa0806d325ecd6e11667c4aab7e5de68
Ankit Gaurav <ankitgaurav@outlook.com>
2018-04-20 06:23:16 +0530

    Add middleware to fetch match based in parameter

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -5,16 +5,19 @@ const _ = require('lodash');
 let items = require('../mocks/items');
 let users = require('../mocks/users');
 
+router.param("id", (req, res, next)=>{
+    var matchedItem = _.find(items, function(o) { return o._id == req.params.id; });
+    if (matchedItem == undefined) return res.status(400).json(matchedItem);
+    req.item = matchedItem;
+    return next();
+});
 
 router.get("/items", (req, res)=>{
     return res.status(200).json(items);    
 });
 
 router.get("/items/:id", (req, res)=>{
-    var matchedItem = _.find(items, function(o) { return o._id == req.params.id; });
-    return (matchedItem == undefined)
-    ? res.status(400).json(matchedItem)
-    : res.status(200).json(matchedItem);
+    return res.status(200).json(req.item);
 });
 
 router.post("/items", (req, res)=>{
@@ -27,18 +30,16 @@ router.put("/items/:id", (req, res)=>{
     for (let el of items) {
         if (el._id == req.params.id) {
             el.detail = req.body.detail;
+            req.item = el;
             return res.status(200).json(el);
         }
     }
-    return res.status(400).json(undefined);
 });
 
 router.delete("/items/:id", (req, res)=>{
-    var matchedItem = _.find(items, function(o) { return o._id == req.params.id; });
-    if (matchedItem == undefined) return res.status(400).json(matchedItem);
-    let newItems = _.remove(items, function(el) { return el._id != req.params.id; });
+    let newItems = _.remove(items, function(el) { return el._id != req.item._id; });
     items = newItems;
-    return res.status(200).json(matchedItem);
+    return res.status(200).json(req.item);
 });
 
 
