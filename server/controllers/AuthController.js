5d97477b664ebe88db07e317a41838762f2a9ac9
Ankit Gaurav <ankitgaurav@outlook.com>
2018-05-22 21:29:18 +0530

    Add authentication middleware

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,11 +1,16 @@
+const createError = require('http-errors');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-function jwtSign(user) {
-    const ONE_WEEK = 60 * 60 * 24 * 7;
-    return jwt.sign(user, "my_jwt_secret", {
-        expiresIn: ONE_WEEK
-    });
+async function jwtSign(payload) {
+    try {
+        const token = await jwt.sign({data: payload}, process.env.JWT_SECRET, { expiresIn: '1h' });
+        console.log(`Token generated sucessfully.`);
+        return token;
+    } catch (err) {
+        console.log(`Error during token generation.`);
+        return err;
+    } 
 }
 
 module.exports = {
@@ -33,13 +38,29 @@ module.exports = {
                 return res.status(403).send({error: `Invalid password.`});
             }
             const userJson = user.toJSON();
-            return res.status(200).send({
-                user: userJson,
-                token: jwtSign(userJson)
-            });
+            try {
+                const token = await jwtSign(user._id.toJSON());
+                return res.status(200).send({ user: userJson, token: token });
+            } catch (err) {
+                return next(createError(500, `Token generation failed.`));
+            }
         } catch (err) {
             console.log(err);
-            return res.status(400).send({error: 'Oops'});
+            return next(createError(500, `Something went wrong in the server.`));
+        }
+    },
+    async isAuthenticated (req, res, next) {
+        const tokenFromUser = req.headers['x-access-token'];
+        if (tokenFromUser == null || tokenFromUser == undefined){
+            return next(createError(401, `Token missing.`));
+        }
+        try{
+            const isAuth = await jwt.verify(tokenFromUser , process.env.JWT_SECRET);
+            console.log(`JWT verified.`);
+            return next();
+        } catch (err) {
+            console.log(`Error while verify jwt`, err);
+            return next(createError(401, `Invalid credentials.`));
         }
     }
 }
