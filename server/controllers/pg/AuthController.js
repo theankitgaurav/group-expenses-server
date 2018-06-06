bef47e8af04c04b06c30cf27f4da89f84ab02a4e
Ankit Gaurav <ankitgaurav@outlook.com>
2018-06-06 13:15:53 +0530

    Complete login api for pg

--- server/controllers/pg/AuthController.js
+++ server/controllers/pg/AuthController.js
@@ -1,36 +1,5 @@
 const createError = require('http-errors');
-const jwt = require('jsonwebtoken');
 const UserService = require('../../services/UserService');
-const Group = require('../../db/models').Group;
-
-/**
- * Asynchronous method to generate a jwt based on the default algorithm HS256
- * The secret key is fetched from environment variable
- * TODO: Change the secret key in production before release
- * @param {any} payload 
- * @returns the generated jwt or Error (eg: TokenExpiredError, JsonWebTokenError)
- */
-async function jwtSign(payload) {
-    try {
-        const token = await jwt.sign({data: payload}, process.env.JWT_SECRET, { expiresIn: '1h' });
-        console.log(`Token generated sucessfully.`);
-        return token;
-    } catch (err) {
-        console.error(`Error during token generation.`);
-        throw err;
-    } 
-}
-/**
- * Function to create a default group for the newly registered 
- * user. The name, type of the group will be personal.
- *
- * @param {*} newUser
- */
-function createDefaultGroup (newUser) {
-    return new Promise(function(resolve, reject) {
-        Group.create({})
-    });
-};
 
 module.exports = {
     async register(req, res, next) {
@@ -47,6 +16,16 @@ module.exports = {
         }
     },
     async login(req, res, next) {
-        res.send('User logged in.');
+        const {email, password} = req.body;
+        try {
+            const result = await UserService.loginUser({email, password});
+            return res.status(200).json({
+                "message": "User login success",
+                "data": {user: result.user, token: result.token}
+            });
+        } catch (err) {
+            console.log(err);
+            return next(err);
+        }
     }
 }
--- server/services/UserService.js
+++ server/services/UserService.js
@@ -1,6 +1,40 @@
+const createError = require('http-errors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const db = require('../db/models/index');
 const User = require('../db/models').User;
 const Group = require('../db/models').Group;
+
+
+/**
+ * Asynchronous method to generate a jwt based on the default algorithm HS256
+ * The secret key is fetched from environment variable
+ * @param {any} payload 
+ * @returns {Prommise} Generated jwt or Error (eg: TokenExpiredError, JsonWebTokenError)
+ */
+async function jwtSign(payload) {
+  try {
+      const token = await jwt.sign({data: payload}, process.env.JWT_SECRET, { expiresIn: '1h' });
+      console.log(`Token generated sucessfully.`);
+      return token;
+  } catch (err) {
+      console.error(`Error during token generation.`);
+      throw err;
+  } 
+}
+
+/**
+ * Asynchronous method to verify is a plainText string 
+ * has the same hash as the store hash
+ *
+ * @param {*} plainTextPassword
+ * @param {*} hashToMatchWith
+ * @returns {Prommise} Valid or not boolean
+ */
+async function isValidPassword(plainTextPassword, hashToMatchWith) {
+  return await bcrypt.compare(plainTextPassword, hashToMatchWith);
+}
+
 /**
  * Helper function to validate user form for registration
  * TODO: Use JOI to validate registration data properly
@@ -11,6 +45,20 @@ function isRegisterDataValid (userObj) {
   if(userObj.name == "" || userObj.email == "" || userObj.password == "") return false;
   return true;
 }
+
+/**
+ * Helper function to validate user form for login
+ * TODO: Use JOI to validate registration data properly
+ * @param {*} userObj
+ * @throws Error if not valid else simply return
+ */
+function validateUserLoginData(userObj) {
+  if(userObj.email == "" || userObj.password == ""){
+    throw new Error ("Incorrect form data for user login.")
+  }
+}
+
+
 module.exports = {
   registerUser: (userObj)=>{
     if (!isRegisterDataValid(userObj)) {
@@ -42,5 +90,48 @@ module.exports = {
       });
 
     });
+  },
+
+  loginUser: (userObj) => {
+    const {email, password} = userObj;
+    validateUserLoginData(userObj);
+
+    let matchedUser = null;
+
+    return new Promise((resolve, reject)=>{
+      User.findOne({ where: {email: email} })
+      .then((user)=>{
+        if(!user) throw new Error ("User does not exist.");
+        console.log("User matched: " + user.email);
+        return matchedUser = user;
+      })
+      .catch((err)=>{
+        console.log(err);
+        return createError(403, err.message);
+      })
+      .then((user)=>{
+        return isValidPassword(password, user.password);
+      })
+      .then((isValidPassword)=>{
+        if(!isValidPassword) throw new Error ('Invalid password given');
+        console.log("Password matched");
+        return matchedUser;
+      })
+      .catch ((err)=>{
+        console.log(err);
+        return createError(403, err.message);
+      })
+      .then ((matchedUser)=> {
+        return jwtSign(matchedUser.id);
+      })
+      .then ((token) => {
+        console.log("Token: " + token);
+        resolve({user: matchedUser, token: token});
+      })
+      .catch ((err) => {
+        console.log(err);
+        reject(createError(500, "Token generation failed"));
+      });
+    })
   }
 };
