18ea0f0c1250a057694937763dd81912fb620974
Ankit Gaurav <ankitgaurav@outlook.com>
2018-07-24 21:36:21 +0530

    Improve router, add logic for jwt invalidation as comment

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -28,13 +28,22 @@ module.exports = {
             return next(err);
         }
     },
+    async logout (req, res, next) {
+        const tokenFromUser = req.headers['x-access-token'];
+        if (tokenFromUser) {
+            await utils.jwtInvalidate(tokenFromUser);
+        }
+        return res.status(200).json({
+            "message": "Logout success"
+        });
+    },
     async isAuthenticated (req, res, next) {
         const tokenFromUser = req.headers['x-access-token'];
         if (tokenFromUser == null || tokenFromUser == undefined){
             return next(new errors.MissingTokenError());
         }
 
-        utils.jwtVerify(tokenFromUser , process.env.JWT_SECRET)
+        utils.jwtVerify(tokenFromUser)
         .then((decoded)=>{
             return decoded;
         })
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const ExpenseController = require('../controllers/ExpenseController');
 
 // Log each request body for debugging
 router.all('*', function (req, res, next) {
+    debug("REQUEST QUERY: ", req.query);
     debug("REQUEST PARAMS: ", req.params);
     debug("REQUEST BODY: ", req.body);
     next();
@@ -16,12 +17,13 @@ router.all('*', function (req, res, next) {
 router.get('/', (req, res, next)=>{ res.status(200).json({"msg": 'Hello from group expenses.'}); });
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
+router.delete("/logout", AuthController.logout);
 
 router.get("/group", AuthController.isAuthenticated, GroupController.getGroups);
 router.get("/group/:groupId", AuthController.isAuthenticated, GroupController.getGroupById);
 router.post("/group/", AuthController.isAuthenticated, GroupController.createGroup);
-router.delete("/group/:groupId", AuthController.isAuthenticated, GroupController.isModifiable, GroupController.deleteGroupById);
-router.get("/group/:groupId/expense", AuthController.isAuthenticated, GroupController.isAuthorized, ExpenseController.getExpensesByGroupId);
+router.delete("/group/:groupId", AuthController.isAuthenticated, GroupController.deleteGroupById);
+router.get("/group/:groupId/expense", AuthController.isAuthenticated, ExpenseController.getExpensesByGroupId);
 
 router.get("/expense", AuthController.isAuthenticated, ExpenseController.getExpensesForUser);
 router.get("/expense/:expenseId", AuthController.isAuthenticated, ExpenseController.getExpenseById);
